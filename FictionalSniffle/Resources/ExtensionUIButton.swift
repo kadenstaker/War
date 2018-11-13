b2455f6f580aa95de031646dd392e9a929190d89
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-11-13 13:50:22 -0700

    Adds UIExtensions, and completed Storyboard with constraints

---
+++ FictionalSniffle/Resources/ExtensionUIButton.swift
@@ -0,0 +1,21 @@
+//
+//  ExtensionUIButton.swift
+//  FictionalSniffle
+//
+//  Created by Ivan Ramirez on 11/13/18.
+//  Copyright © 2018 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    @IBInspectable
+    var rotation: Int {
+        get {
+            return 0
+        } set {
+            let radians = CGFloat(CGFloat(Double.pi) * CGFloat(newValue) / CGFloat(180.0))
+            self.transform = CGAffineTransform(rotationAngle: radians)
+        }
+    }
+}
