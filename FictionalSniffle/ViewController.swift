06ed719c53a153284dc98a137c1d02c9139c4888
Kaden Staker <kadenstaker@kadens-macbook-pro.local>
2018-11-13 10:16:02 -0700

    Initial Commit

---
+++ FictionalSniffle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FictionalSniffle
+//
+//  Created by Kaden Staker on 11/13/18.
+//  Copyright © 2018 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
