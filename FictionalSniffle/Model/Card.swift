51e383c7bb201ce583094eb1763c4e1837d03990
Kaden Staker <kadenstaker@Kadens-MacBook-Pro.local>
2018-11-13 15:27:47 -0700

    adds model and model controller

--- FictionalSniffle/Model/Card.swift
+++ FictionalSniffle/Model/Card.swift
@@ -16,4 +16,28 @@ struct Card: Decodable {
     let suit: String
     let image: String
     let value: String
+    
+    var rank: Int {
+        if let integerValue = Int(value) {
+            return integerValue
+        }
+        switch value {
+        case "ACE":
+            return 1
+        case "JACK":
+            return 11
+        case "QUEEN":
+            return 12
+        case "KING":
+            return 13
+        default:
+            return 0
+        }
+    }
+}
+
+extension Card: Comparable {
+    static func < (lhs: Card, rhs: Card) -> Bool {
+        return lhs.rank < rhs.rank
+    }
 }
